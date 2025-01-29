b8d8779a3e3fe45508af55000bb00163cec26531
Shivakant <kants6397@gmail.com>
2025-01-29 12:53:53 +0530

    add model

---
+++ src/model/claim.model.js
@@ -0,0 +1,13 @@
+class Claim {
+  constructor(id, name, description, status, date, userId) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.status = status;
+    this.amount = amount;
+    this.date = date;
+    this.userId = userId;
+  }
+}
+
+export default Claim;
