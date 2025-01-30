d8977a768255076fd809c846910c47a20518b066
Shivakant <kants6397@gmail.com>
2025-01-30 13:04:23 +0530

    add claim record funtions to keep tracks of the claims

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,17 +1,21 @@
 import { claim_db } from "../model/claim.model.js";
+import { ClaimRecords, claim_records_db } from "../model/claimRecords.model.js";
 
 //function to approve a claim
 export const approveClaim = (req, res) => {
   const { id } = req.params;
+
   try {
     if (!claim_db[id]) {
       return res.status(404).send({ message: "Claim not found" });
     }
     claim_db[id].status = "approved";
+    const claimRecord = new ClaimRecords(claim_db[id]);
+    claim_records_db[claimRecord.id] = claimRecord;
     return res.status(200).json({
       success: true,
       message: "Claim approved successfully",
-      claim: claim_db[id],
+      claim: claim_records_db[claimRecord.id],
     });
   } catch (error) {
     return res.status(500).send({ message: error.message });
@@ -26,10 +30,12 @@ export const rejectClaim = (req, res) => {
       return res.status(404).send({ message: "Claim not found" });
     }
     claim_db[id].status = "rejected";
+    const claimRecord = new ClaimRecords(claim_db[id]);
+    claim_records_db[claimRecord.id] = claimRecord;
     return res.status(200).json({
       success: true,
-      message: "Claim rejected successfully",
-      claim: claim_db[id],
+      message: "Claim rejected",
+      claim: claim_records_db[claimRecord.id],
     });
   } catch (error) {
     return res.status(500).send({ message: error.message });
