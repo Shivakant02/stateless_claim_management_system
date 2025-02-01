aea28dd7efecb9141a9febc07816058fe629ce5e
Shivakant <kants6397@gmail.com>
2025-02-01 12:38:43 +0530

    add submit claim feature

--- src/controllers/claim.controller.js
+++ src/controllers/claim.controller.js
@@ -1,103 +1,51 @@
-import { Claim, claim_db } from "../model/claim.model.js";
+import Claim from "../model/claim.model.js";
+import Policy from "../model/policy.model.js";
+import User from "../model/user.model.js";
+import AppError from "../utils/AppError.js";
 
-// function to submit the claim
-export const submitClaim = (req, res) => {
-  try {
-    if (
-      !req.body.user_id ||
-      !req.body.policyNumber ||
-      !req.body.dateOfLoss ||
-      !req.body.lossDescription ||
-      !req.body.lossAmount
-    ) {
-      return res.status(400).send({ message: "All fields are required" });
-    }
+export const submitClaim = async (req, res,next) =>{
+try {
+  const userId = req.user.id;
+  const user=await User.findById(userId);
 
-    if (req.body.lossAmount > 1000000) {
-      return res
-        .status(400)
-        .send({ message: "claimed amount is more than tha coverage amount " });
-    }
-
-    const claim = new Claim(req);
-    claim_db[claim.id] = claim;
-    console.log(claim_db[claim.id]);
-
-    return res.status(201).json({
-      success: true,
-      message: "Claim created successfully",
-      claim,
-    });
-  } catch (error) {
-    res.status(500).send({ message: error.message });
+  if(!user){
+    return next(new AppError("User not found", 404)); 
   }
-};
 
-//funtion to get the claim
-export const getClaim = (req, res) => {
-  const { id } = req.params;
-  try {
-    if (!id) {
-      return res.status(400).send({ message: "claim id required" });
-    }
-    if (!claim_db[id]) {
-      return res.status(404).send({ message: "Claim not found" });
-    }
-    return res.status(200).json({
-      success: true,
-      message: "Claim retrieved successfully",
-      claim: claim_db[id],
-    });
-  } catch (error) {
-    return res.status(500).send({ message: error.message });
+  
+
+  const policyId=req.params.id;
+  // console.log(policyId);
+  
+  const policy=await Policy.findById(policyId);
+  if(!policy){
+    return next(new AppError("Policy not found", 404));
   }
-};
 
-//function to update the claim details
-export const updateClaim = (req, res) => {
-  const { id } = req.params;
-  const { user_id, dateOfLoss, lossDescription, lossAmount } = req.body;
-  // console.log(id);
-  // console.log(claim_db[id]);
-  try {
-    if (!claim_db[id]) {
-      return res.status(404).send({ message: "Claim not found" });
-    }
-    if (user_id != claim_db[id].user_id) {
-      return res
-        .status(403)
-        .send({ message: "user does not have permission to update the claim" });
-    }
-    if (dateOfLoss) claim_db[id].dateOfLoss = dateOfLoss;
-    if (lossDescription) claim_db[id].lossDescription = lossDescription;
-    if (lossAmount) claim_db[id].lossAmount = lossAmount;
+if(policy.policyHolder!==userId){
+  return next(new AppError("You are not authorized to submit claim for this policy", 401));
+}
 
-    return res.status(200).json({
-      success: true,
-      message: "Claim updated successfully",
-      claim: claim_db[id],
-    });
-  } catch (error) {
-    return res.status(500).send({ message: error.message });
+console.log(policy);
+  const {claimAmount,claimReason}=req.body;
+  if (!claimAmount || !claimReason) {
+    return next(new AppError("Please provide all required fields", 400));
   }
-};
 
-// function to delete the claim
-export const deleteClaim = (req, res) => {
-  const { user_id, id } = req.params;
-  try {
-    if (claim_db[id].user_id != user_id) {
-      return res
-        .status(403)
-        .send({ message: "user does not have permission to delete" });
-    }
-    if (!claim_db[id]) {
-      return res.status(404).send({ message: "Claim not found" });
-    }
+  const claim = await Claim.create({
+    policyId:policy._id,
+    claimAmount:claimAmount,
+    claimReason:claimReason,
+    policyHolder:user._id
+  });
+ 
+  return res.status(201).json({
+    success: true,
+    message: "Claim submitted successfully",
+    claim,
+  });
 
-    delete claim_db[id];
-    return res.status(200).send({ message: "Claim deleted successfully" });
-  } catch (error) {
-    return res.status(500).send({ message: error.message });
-  }
-};
+} catch (error) {
+  return next(new AppError(error.message, 500));
+}
+} 
--- src/routes/claim.routes.js
+++ src/routes/claim.routes.js
@@ -1,17 +1,10 @@
 import { Router } from "express";
-import {
-  deleteClaim,
-  getClaim,
-  submitClaim,
-  updateClaim,
-} from "../controllers/claim.controller.js";
+import {submitClaim,} from "../controllers/claim.controller.js";
+import { verifyToken } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 // Route to handle Claims
-router.post("/submitClaim", submitClaim);
-router.get("/getClaim/:id", getClaim);
-router.put("/updateClaim/:id", updateClaim);
-router.delete("/delete/:user_id/:id", deleteClaim);
+router.post("/submitClaim/:id",verifyToken, submitClaim);
 
 export default router;
