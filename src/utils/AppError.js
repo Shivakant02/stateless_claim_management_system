37ab631ff92fe27fc44b2d3b4277dd1d85ef52c2
Shivakant <kants6397@gmail.com>
2025-02-03 18:31:22 +0530

    app error refactor

--- src/utils/AppError.js
+++ src/utils/AppError.js
@@ -2,6 +2,8 @@ class AppError extends Error {
     constructor(message, statusCode) {
       super(message);
       this.statusCode = statusCode;
+      this.success = false;
+      this.message = message;
   
       Error.captureStackTrace(this, this.constructor);
     }
