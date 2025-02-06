fa04103a19662aa059289f3a2400477d91881848
Shivakant <kants6397@gmail.com>
2025-02-06 10:29:09 +0530

    set cookie options for the deployment

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,9 @@ import AppError from "../utils/AppError.js"
 //cookie options
 const cookieOptions = {
   expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-  httpOnly: true,
+  httpOnly: false,
+  secure: true,
+  sameSite: "none",
 };
 
 //signup funtion
